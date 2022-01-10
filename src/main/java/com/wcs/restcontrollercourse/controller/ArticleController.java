544b5834a78236108e246f178d09765318c6c93f
PierreP <paillard.pierre@gmail.com>
2022-01-10 16:12:35 +0100

    adding coms

--- src/main/java/com/wcs/restcontrollercourse/controller/ArticleController.java
+++ src/main/java/com/wcs/restcontrollercourse/controller/ArticleController.java
@@ -2,4 +2,14 @@ package com.wcs.restcontrollercourse.controller;
 
 public class ArticleController {
 
+	// Get all articles
+	
+	// Get one article
+	
+	// Create article
+	
+	// Update article
+	
+	// Delete article
+	
 }
